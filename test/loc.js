1bf5230fe681db7a188c6e844d3d8b4e487d1d42
James Halliday <mail@substack.net>
2015-04-15 03:02:12 -0700

    some more tests

--- test/loc.js
+++ test/loc.js
@@ -9,3 +9,15 @@ test('3pm in tashkent to santiag time', function (t) {
         t.equal(strftime('%T', rows[0].date), '06:00:00');
     });
 });
+
+test('london to moscow', function (t) {
+    t.plan(4);
+    tzloc('5:30 pm aug 22 1980', 'london', 'moscow', function (err, rows) {
+        t.ifError(err);
+        t.equal(strftime('%F %T', rows[0].date), '1980-08-22 19:30:00');
+    });
+    tzloc('19:30 aug 22 1980', 'moscow', 'london', function (err, rows) {
+        t.ifError(err);
+        t.equal(strftime('%F %T', rows[0].date), '1980-08-22 17:30:00');
+    });
+});
